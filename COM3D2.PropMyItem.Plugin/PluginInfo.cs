32eb595ea8cc80e7a897f8268962538f1ea0df43
InoryS <101243352+InoryS@users.noreply.github.com>
2023-08-23 02:57:00 +0800

    Update PluginInfo.cs

--- COM3D2.PropMyItem.Plugin/PluginInfo.cs
+++ COM3D2.PropMyItem.Plugin/PluginInfo.cs
@@ -6,10 +6,10 @@ namespace COM3D2.PropMyItem.Plugin
     public struct PluginInfo
     {
         // Token: 0x04000025 RID: 37
-        public const string PluginName = "PropMyItem";
+        public const string PluginName = "PropMyItem-Inory";
 
         // Token: 0x04000026 RID: 38
-        public const string PluginVersion = "2.4.0.1";
+        public const string PluginVersion = "2.4.0.2";
 
         // Token: 0x04000027 RID: 39
         public const int WindowID = 4511;
